1c6ba2369be98ff68671bec7ddb0807d6e1ecf40
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-05-19 10:59:10 +0900

    blog-app

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,31 +2,41 @@ class BlogsController < ApplicationController
   before_action :move_to_index, except: [:index ,:show]
 
   def index
-    @blogs = Blog.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+   @blogs = Blog.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
+    @blog = Blog.new
   end
 
   def create
-    Blog.create()
+    Blog.create(blog: blog_params[:blog], user_id: current_user.id)
+    
   end
   
   def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy if blog.user_id == current_user.id
   end
 
   def update
+    blog = Blog.find(params[:id])
+    if blog.user_id == current_user.id
+      blog.update(blog_params)
+    end
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   private
     def blog_params
-      params.permit()
+      params.require(:blog).permit(:blog, :id)
     end
 
     def move_to_index
@@ -34,5 +44,4 @@ class BlogsController < ApplicationController
         redirect_to action: :index
       end
     end
-    
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(params[:id])
-    @blog = user.glogs.page(params[:page]).per(5).order("created_at DESC")
+    # user = User.find(params[:id])
+    # @blog = user.glogs.page(params[:page]).per(5).order("created_at DESC")
   end
   
 end
